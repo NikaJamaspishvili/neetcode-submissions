a95db3aa6495a1e913177449f75353478b1ea327
Nika jamaspishvili <112616706+NikaJamaspishvili@users.noreply.github.com>
2026-09-03 19:08:18 +0400

    Add: valid-sudoku - submission-0

---
+++ Data Structures & Algorithms/valid-sudoku/submission-0.js
@@ -0,0 +1,25 @@
+class Solution {
+    isValidSudoku(board) {
+        let rows = Array.from({ length: 9 }, () => new Set());
+        let cols = Array.from({ length: 9 }, () => new Set());
+        let boxes = Array.from({ length: 9 }, () => new Set());
+
+        for (let r = 0; r < 9; r++) {
+            for (let c = 0; c < 9; c++) {
+                let val = board[r][c];
+                if (val === ".") continue;
+
+                let boxIdx = Math.floor(r / 3) * 3 + Math.floor(c / 3);
+
+                if (rows[r].has(val) || cols[c].has(val) || boxes[boxIdx].has(val)) {
+                    return false;
+                }
+
+                rows[r].add(val);
+                cols[c].add(val);
+                boxes[boxIdx].add(val);
+            }
+        }
+        return true;
+    }
+}
